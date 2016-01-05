914c8cf012916b7e001c0e893dc1f5f868ce0513
jsdevel <js.developer.undefined@gmail.com>
2016-01-04 17:19:19 -0700

    initial

---
+++ test/data-driven/throw-error-when-args-is-missing.js
@@ -0,0 +1,13 @@
+module.exports = {
+  constructorError: /express-openapi-defaults: missing args argument/,
+
+  args: null,
+
+  requestPath: '',
+
+  requestHeaders: null,
+
+  headers: null,
+  params: null,
+  query: null
+};
