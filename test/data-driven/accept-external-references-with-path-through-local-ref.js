d7588692e143bcb881132828f43382b9f8197b1f
TANAKA Koichi <mugeso@mugeso.com>
2016-04-02 00:19:06 +0900

    Support external schemas
    See https://github.com/kogosoftwarellc/express-openapi/issues/17

---
+++ test/data-driven/accept-external-references-with-path-through-local-ref.js
@@ -0,0 +1,38 @@
+module.exports = {
+    constructorArgs: {
+        responses: {
+            200: {
+                schema: {
+                    type: 'object',
+                    properties: {
+                        foo: {
+                            $ref: '#/definitions/foo'
+                        }
+                    }
+                }
+            }
+        },
+
+        definitions: {
+            foo: {
+                $ref: 'http://example.com/schema#/definitions/foo'
+            }
+        },
+
+        externalSchemas: {
+            'http://example.com/schema': {
+                definitions: {
+                    foo: {
+                        type: 'string'
+                    }
+                }
+            }
+        }
+    },
+
+    inputStatusCode: 200,
+    inputResponseBody: {foo: 'asdf'},
+
+    expectedValidationError: ''
+};
+
