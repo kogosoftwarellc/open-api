b19c02bbb9aa583292b156a2d93236d95c75369b
Tim Willis <willist@users.noreply.github.com>
2016-05-12 12:25:02 -0500

    Update basePath at request time.
    * Update basePath at request time.

---
+++ test/sample-projects/with-express-mount-deeply-nested/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+// normally you'd just do require('express-openapi'), but this is for test purposes.
+var openapi = require('../../../');
+var path = require('path');
+var cors = require('cors');
+
+var grandparentApp = express();
+var parentApp = express();
+var app = express();
+grandparentApp.use(cors());
+grandparentApp.use(bodyParser.json());
+
+openapi.initialize({
+  apiDoc: require('./api-doc.js'),
+  app: app,
+  routes: path.resolve(__dirname, 'api-routes')
+});
+
+parentApp.use(function(err, req, res, next) {
+  res.status(err.status).json(err);
+});
+
+
+parentApp.use('/parent', app);
+grandparentApp.use('/grandparent', parentApp);
+
+module.exports = grandparentApp;
+
+var port = parseInt(process.argv[2]);
+if (port) {
+  parentApp.listen(port);
+}
