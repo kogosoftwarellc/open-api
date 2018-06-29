55afbd82a928bd8dfc75fe75c0e204086222d0da
jsdevel <js.developer.undefined@gmail.com>
2018-06-29 01:34:41 -0700

    Adding openapi-types and typescript
    * Also adding bin/publish

---
+++ packages/openapi-types/README.md
@@ -0,0 +1,50 @@
+# openapi-types [![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Coveralls Status][coveralls-image]][coveralls-url] [![Gitter chat][gitter-image]][gitter-url]
+> Types for OpenAPI documents.
+
+## Usage
+
+```typescript
+import { OpenAPIV2, OpenAPIV3 } from "openapi-types";
+
+function processV2(doc: OpenAPIV2.Document) {}
+
+function processV3(doc: OpenAPIV3.Document) {}
+```
+
+## LICENSE
+``````
+The MIT License (MIT)
+
+Copyright (c) 2018 Kogo Software LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+``````
+
+[downloads-image]: http://img.shields.io/npm/dm/openapi-types.svg
+[npm-url]: https://npmjs.org/package/openapi-types
+[npm-image]: http://img.shields.io/npm/v/openapi-types.svg
+
+[travis-url]: https://travis-ci.org/kogosoftwarellc/open-api
+[travis-image]: http://img.shields.io/travis/kogosoftwarellc/open-api.svg
+
+[coveralls-url]: https://coveralls.io/r/kogosoftwarellc/open-api
+[coveralls-image]: http://img.shields.io/coveralls/kogosoftwarellc/open-api/master.svg
+
+[gitter-url]: https://gitter.im/kogosoftwarellc/open-api
+[gitter-image]: https://badges.gitter.im/kogosoftwarellc/open-api.png
---
+++ packages/openapi-types/index.ts
@@ -0,0 +1,305 @@
+export namespace OpenAPIV3 {
+  export interface Document {
+  }
+}
+
+export namespace OpenAPIV2 {
+  export interface Document {
+    basePath?: string
+    consumes?: MimeTypes
+    definitions?: DefinitionsObject
+    externalDocs?: ExternalDocumentationObject
+    host?: string
+    info: InfoObject
+    parameters?: ParametersDefinitionsObject
+    paths: PathsObject
+    produces?: MimeTypes
+    responses?: ResponsesDefinitionsObject
+    schemes?: string[]
+    security?: SecurityRequirementObject[]
+    securityDefinitions?: SecurityDefinitionsObject
+    swagger: string
+    tags?: TagObject[]
+  }
+
+  export interface TagObject {
+    name: string
+    description?: string
+    externalDocs?: ExternalDocumentationObject
+  }
+
+  interface SecuritySchemeObjectBase {
+    type: 'basic' | 'apiKey' | 'oauth2'
+    description?: string
+  }
+
+  interface SecuritySchemeBasic extends SecuritySchemeObjectBase {
+    type: 'basic'
+  }
+
+  interface SecuritySchemeApiKey extends SecuritySchemeObjectBase {
+    type: 'apiKey'
+    name: string
+    in: string
+  }
+
+  type SecuritySchemeOauth2 = SecuritySchemeOauth2Implicit | SecuritySchemeOauth2AccessCode |
+    SecuritySchemeOauth2Password | SecuritySchemeOauth2Application;
+
+  export interface ScopesObject {
+    [index: string]: any
+  }
+
+  interface SecuritySchemeOauth2Base extends SecuritySchemeObjectBase {
+    flow: 'implicit' | 'password' | 'application' | 'accessCode'
+    scopes: ScopesObject
+  }
+
+  interface SecuritySchemeOauth2Implicit extends SecuritySchemeOauth2Base {
+    flow: 'implicit'
+    authorizationUrl: string
+  }
+
+  interface SecuritySchemeOauth2AccessCode extends SecuritySchemeOauth2Base {
+    flow: 'accessCode'
+    authorizationUrl: string
+    tokenUrl: string
+  }
+
+  interface SecuritySchemeOauth2Password extends SecuritySchemeOauth2Base {
+    flow: 'password'
+    tokenUrl: string
+  }
+
+  interface SecuritySchemeOauth2Application extends SecuritySchemeOauth2Base {
+    flow: 'application'
+    tokenUrl: string
+  }
+
+  export type SecuritySchemeObject = SecuritySchemeBasic | SecuritySchemeApiKey | SecuritySchemeOauth2;
+
+  export interface SecurityDefinitionsObject {
+    [index: string]: SecuritySchemeObject
+  }
+
+  export interface SecurityRequirementObject {
+    [index: string]: string[]
+  }
+
+  export interface ReferenceObject {
+    $ref: string
+  }
+
+  type Response = ResponseObject|ReferenceObject
+
+  export interface ResponsesDefinitionsObject {
+    [index: string]: ResponseObject
+  }
+
+  type Schema = SchemaObject | ReferenceObject
+
+  export interface ResponseObject {
+    description: string
+    schema?: Schema
+    headers?: HeadersObject
+    examples?: ExampleObject
+  }
+
+  export interface HeadersObject {
+    [index: string]: HeaderObject
+  }
+
+  export interface HeaderObject extends ItemsObject {
+  }
+
+  export interface ExampleObject {
+    [index: string]: any
+  }
+
+  export interface ResponseObject {
+    description: string
+    schema?: Schema
+    headers?: HeadersObject
+    examples?: ExampleObject
+  }
+
+  export interface OperationObject {
+    tags?: string[]
+    summary?: string
+    description?: string
+    externalDocs?: ExternalDocumentationObject
+    operationId?: string
+    consumes?: MimeTypes
+    produces?: MimeTypes
+    parameters?: Parameters
+    responses: ResponsesObject
+    schemes?: string[]
+    deprecated?: boolean
+    security?: SecurityRequirementObject[],
+    [index: string]: any
+  }
+
+  export interface ResponsesObject {
+    [index: string]: Response|any
+    'default': Response
+  }
+
+  type Parameters = (ReferenceObject|Parameter)[]
+
+  type Parameter = (InBodyParameterObject|GeneralParameterObject);
+
+  export interface InBodyParameterObject extends ParameterObject {
+    schema: Schema
+  }
+
+  export interface GeneralParameterObject extends ParameterObject, ItemsObject {
+    allowEmptyValue?: boolean
+  }
+
+  export interface PathItemObject {
+    $ref?: string
+    get?: OperationObject
+    put?: OperationObject
+    post?: OperationObject
+    del?: OperationObject
+    'delete'?: OperationObject
+    options?: OperationObject
+    head?: OperationObject
+    patch?: OperationObject
+    parameters?: Parameters
+  }
+
+  export interface PathsObject {
+    [index: string]: PathItemObject|any
+  }
+
+  export interface ParametersDefinitionsObject {
+    [index: string]: ParameterObject
+  }
+
+  interface ParameterObject {
+    name: string
+    'in': string
+    description?: string
+    required?: boolean
+    [index: string]: any
+  }
+
+  export type MimeTypes = string[]
+
+  export interface DefinitionsObject {
+    [index: string]: SchemaObject
+  }
+
+  export interface SchemaObject extends IJsonSchema {
+    [index: string]: any
+    discriminator?: string
+    readOnly?: boolean
+    xml?: XMLObject
+    externalDocs?: ExternalDocumentationObject
+    example?: any
+    default?: any
+    items?: ItemsObject
+    properties?: {
+      [name: string]: SchemaObject
+    }
+  }
+
+  export interface ExternalDocumentationObject {
+    [index: string]: any
+    description?: string
+    url: string
+  }
+
+  export interface ItemsObject {
+    type: string
+    format?: string
+    items?: ItemsObject
+    collectionFormat?: string
+    'default'?: any
+    maximum?: number
+    exclusiveMaximum?: boolean
+    minimum?: number
+    exclusiveMinimum?: boolean
+    maxLength?: number
+    minLength?: number
+    pattern?: string
+    maxItems?: number
+    minItems?: number
+    uniqueItems?: boolean
+    'enum'?: any[]
+    multipleOf?: number
+    $ref?: string
+  }
+
+  export interface XMLObject {
+    [index: string]: any
+    name?: string
+    namespace?: string
+    prefix?: string
+    attribute?: boolean
+    wrapped?: boolean
+  }
+
+  export interface InfoObject {
+    title: string
+    description?: string
+    termsOfService?: string
+    contact?: ContactObject
+    license?: LicenseObject
+    version: string
+  }
+
+  export interface ContactObject {
+    name?: string
+    url?: string
+    email?: string
+  }
+
+  export interface LicenseObject {
+    name: string
+    url?: string
+  }
+}
+
+export interface IJsonSchema {
+  id?: string
+  $schema?: string
+  title?: string
+  description?: string
+  multipleOf?: number
+  maximum?: number
+  exclusiveMaximum?: boolean
+  minimum?: number
+  exclusiveMinimum?: boolean
+  maxLength?: number
+  minLength?: number
+  pattern?: string
+  additionalItems?: boolean | IJsonSchema
+  items?: IJsonSchema | IJsonSchema[]
+  maxItems?: number
+  minItems?: number
+  uniqueItems?: boolean
+  maxProperties?: number
+  minProperties?: number
+  required?: string[]
+  additionalProperties?: boolean | IJsonSchema
+  definitions?: {
+    [name: string]: IJsonSchema
+  }
+  properties?: {
+    [name: string]: IJsonSchema
+  }
+  patternProperties?: {
+    [name: string]: IJsonSchema
+  }
+  dependencies?: {
+    [name: string]: IJsonSchema | string[]
+  }
+  'enum'?: any[]
+  type?: string | string[]
+  allOf?: IJsonSchema[]
+  anyOf?: IJsonSchema[]
+  oneOf?: IJsonSchema[]
+  not?: IJsonSchema
+}
