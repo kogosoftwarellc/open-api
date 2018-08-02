e1dc45fa0c80942fe8b1f80b9757d4d084bc757e
Brian Crowell <brian@fluggo.com>
2018-08-02 13:03:56 -0500

    Add tests for OpenAPI 3 query parameters

---
+++ packages/openapi-request-validator/test/data-driven/accept-custom-formats-openapi3.js
@@ -0,0 +1,26 @@
+module.exports = {
+  validateArgs: {
+    parameters: [
+      {
+        in: "query",
+        name: "foo",
+        schema: {
+          type: "string",
+          format: "foo"
+        }
+      }
+    ],
+    schemas: null,
+    customFormats: {
+      foo: function(input) {
+        return input === 'foo'
+      }
+    }
+  },
+  request: {
+    path: "?foo=foo",
+    query: {
+      foo: "foo"
+    }
+  }
+};
