2fe77b2f1fbca27aa422f3489cea662f60dea5db
Brian Crowell <brian@fluggo.com>
2018-08-02 13:03:56 -0500

    Add tests for OpenAPI 3 query parameters

---
+++ packages/openapi-request-validator/test/data-driven/fail-customFormats-for-openapi3-query-param.js
@@ -0,0 +1,38 @@
+module.exports = {
+  "validateArgs": {
+    "parameters": [
+      {
+        "in": "query",
+        "name": "foo",
+        "schema": {
+          "type": "string",
+          "format": "foo",
+        },
+        "required": true,
+      }
+    ],
+    "schemas": null,
+    "customFormats": {
+      foo: function(input) {
+        return input === 'foo'
+      }
+    }
+  },
+  "request": {
+    "path": "?foo=boo",
+    "query": {
+      "foo": "boo"
+    }
+  },
+  "expectedError": {
+    "status": 400,
+    "errors": [
+      {
+        "path": "foo",
+        "errorCode": "format.openapi.validation",
+        "message": "instance.foo does not conform to the \"foo\" format",
+        "location": "query"
+      }
+    ]
+  }
+};
