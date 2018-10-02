bedb343a3274c619520718b6167a5f087ec634dd
jsdevel <js.developer.undefined@gmail.com>
2018-10-01 23:42:54 -0700

    openapi-schema-validator: Moving to typescript (#174)
    * Adding CHANGELOG.md

---
+++ packages/openapi-schema-validator/test/data-driven.ts
@@ -0,0 +1,17 @@
+const expect = require('chai').expect;
+const glob = require('glob');
+const path = require('path');
+const baseDir = path.resolve(__dirname, 'data-driven');
+import Sut from '../';
+
+describe(require('../package.json').name, () => {
+  glob.sync('*.js', {cwd: baseDir}).forEach(fixture => {
+    const testName = path.basename(fixture, '.js').replace(/-/g, ' ');
+    fixture = require(path.resolve(baseDir, fixture));
+    it(`should ${testName}`, () => {
+      const sut = new Sut(fixture.constructorArgs);
+      const errors = sut.validate(fixture.apiDoc).errors;
+      expect(errors).to.deep.equal(fixture.errors);
+    });
+  });
+});
