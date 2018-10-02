65403b4c5989e4e0e638f80218c5bed179336708
jsdevel <js.developer.undefined@gmail.com>
2018-10-01 23:42:54 -0700

    openapi-schema-validator: Moving to typescript (#174)
    * Adding CHANGELOG.md

---
+++ packages/openapi-schema-validator/index.ts
@@ -0,0 +1,50 @@
+import * as Ajv from 'ajv';
+const openapi2Schema = require('swagger-schema-official/schema.json');
+const openapi3Schema = require('./schema/openapi-3.0.json');
+const merge = require('lodash.merge');
+import { OpenAPI, IJsonSchema } from 'openapi-types';
+
+export interface IOpenAPISchemaValidator {
+  /**
+   * Validate the provided OpenAPI doc against this validator's schema version and
+   * return the results.
+   */
+  validate(doc: OpenAPI.Document): OpenAPISchemaValidatorResult;
+}
+
+export interface OpenAPISchemaValidatorArgs {
+  version: number | string
+  extensions?: IJsonSchema
+}
+
+export interface OpenAPISchemaValidatorResult {
+  errors: Array<Ajv.ErrorObject>
+}
+
+export default class OpenAPISchemaValidator implements IOpenAPISchemaValidator {
+  private validator: Ajv.ValidateFunction;
+  constructor(args: OpenAPISchemaValidatorArgs) {
+    const v = new Ajv({schemaId: 'auto', allErrors: true});
+    v.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
+    const version = args && parseInt(String(args.version), 10) || 2;
+    const schema = merge(
+      {},
+      version === 2 ?
+        openapi2Schema :
+        openapi3Schema,
+      args ?
+        args.extensions :
+        {}
+    );
+    v.addSchema(schema);
+    this.validator = v.compile(schema);
+  }
+
+  validate(openapiDoc: OpenAPI.Document): OpenAPISchemaValidatorResult {
+    if (!this.validator(openapiDoc)) {
+      return {errors: this.validator.errors};
+    } else {
+      return {errors: []};
+    }
+  }
+}
