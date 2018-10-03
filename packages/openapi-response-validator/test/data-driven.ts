6d6261d9495ae348f6230645b98880a4952b415a
jsdevel <js.developer.undefined@gmail.com>
2018-10-02 23:27:36 -0700

    openapi-response-validator: Moving to typescript. (#174)
    * Adding CHANGELOG.md

---
+++ packages/openapi-response-validator/test/data-driven.ts
@@ -0,0 +1,25 @@
+const expect = require('chai').expect;
+const glob = require('glob');
+const path = require('path');
+const baseDir = path.resolve(__dirname, 'data-driven');
+import Sut from '../';
+
+describe(require('../package.json').name, () => {
+  glob.sync('*.js', {cwd: baseDir}).forEach(fixture => {
+    const testName = path.basename(fixture, '.js').replace(/-/g, ' ');
+    fixture = require(path.resolve(baseDir, fixture));
+
+    it(`should ${testName}`, () => {
+      if (fixture.constructorError) {
+        expect(() => {
+          new Sut(fixture.constructorArgs);
+        }).to.throw(fixture.constructorError);
+        return;
+      }
+
+      const instance = new Sut(fixture.constructorArgs);
+      const error = instance.validateResponse(fixture.inputStatusCode, fixture.inputResponseBody);
+      expect(error).to.eql(fixture.expectedValidationError);
+    });
+  });
+});
