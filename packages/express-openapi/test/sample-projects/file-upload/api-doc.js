829046a8b412f54b6cf60dd7f56680fc507ef6d4
troy molsberry <troy.molsberry@packetsled.com>
2018-10-05 16:35:23 -0700

    add a unit test for "file" parameter types

---
+++ packages/express-openapi/test/sample-projects/file-upload/api-doc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  swagger: '2.0',
+
+  host: "test-host",
+  basePath: '/v3',
+
+  info: {
+    title: 'express-openapi sample project',
+    version: '3.0.0'
+  },
+
+  definitions: {},
+
+  paths: {},
+
+  tags: [
+    {description: 'Drafts', name: 'Drafts'}
+  ]
+};
---
+++ packages/express-openapi/test/sample-projects/file-upload/spec.js
@@ -0,0 +1,13 @@
+var app;
+var expect = require('chai').expect;
+var request = require('supertest');
+
+before(function() {
+  app = require('./app.js');
+});
+
+it('should upload a file', function(done) {
+  request(app)
+    .post('/v3/fileupload')
+    .expect(200, {}, done);
+});
