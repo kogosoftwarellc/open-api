16b66b2ef62884b178328f3c0193b0cbf634ab46
jsdevel <js.developer.undefined@gmail.com>
2018-10-06 13:46:04 -0700

    openapi-schema-validator: Adding type:file to a valid v2 document test

--- packages/openapi-schema-validator/test/data-driven/accept-valid-v2-documents.js
+++ packages/openapi-schema-validator/test/data-driven/accept-valid-v2-documents.js
@@ -17,7 +17,56 @@ module.exports = {
         }
       }
     },
-    paths: {}
+    paths: {
+      '/foo': {
+        get: {
+          summary: 'Form file upload. Accepts a form file upload named "file".',
+          operationId: 'FormDraftFileUpload',
+
+          tags: ['Drafts'],
+          consumes: [
+            'multipart/form-data'
+          ],
+          parameters: [
+            {
+              name: 'process_intel',
+              in: 'formData',
+              description: 'Whether or not to transform intel files.',
+              type: 'boolean',
+              default: false
+            },
+            {
+              name: 'overwrite',
+              in: 'formData',
+              description: 'Whether or not to replace an existing file.',
+              type: 'boolean',
+              default: true
+            },
+            {
+              name: 'basepath',
+              in: 'formData',
+              description: 'Root directory to place files.',
+              type: 'string',
+              default: ''
+            },
+            {
+              name: 'file',
+              in: 'formData',
+              description: 'The file to upload',
+              type: 'file'
+            }
+          ],
+          responses: {
+            200: {
+              'description': 'OK'
+            },
+            400: {
+              'description': 'BAD'
+            }
+          }
+        }
+      }
+    }
   },
 
   errors: []
