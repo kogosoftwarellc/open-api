8e277a014ca4ed4ee64072fdd0dc9f9eb4a49d3b
jsdevel <js.developer.undefined@gmail.com>
2018-10-06 13:42:15 -0700

    openapi-request-validator: Fixing type:file breaking validation.

---
+++ packages/openapi-request-validator/test/data-driven/accept-file-type-parameters.js
@@ -0,0 +1,18 @@
+module.exports = {
+  validateArgs: {
+    parameters: [
+      {
+        name: 'file',
+        in: 'formData',
+        description: 'The file to upload',
+        type: 'file'
+      }
+    ],
+    schemas: null
+  },
+  request: {
+    headers: {
+      "x-foo": "asdf"
+    }
+  }
+};
