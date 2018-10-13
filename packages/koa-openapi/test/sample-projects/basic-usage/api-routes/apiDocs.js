11ea3505be21a77ae44f8b69366359d1ad97109c
jsdevel <js.developer.undefined@gmail.com>
2018-10-13 00:06:07 -0700

    koa-openapi: Adding basic-usage test. (#113)
    * It is mostly working now.

---
+++ packages/koa-openapi/test/sample-projects/basic-usage/api-routes/apiDocs.js
@@ -0,0 +1,38 @@
+module.exports = {
+  get: get
+};
+
+function get(ctx) {
+  if (ctx.query.type === 'apiDoc') {
+    return ctx.body = ctx.state.apiDoc;
+  }
+  return ctx.body = ctx.state.operationDoc;
+}
+
+get.apiDoc = {
+  operationId: 'getApiDoc',
+  description: 'Returns the requested apiDoc',
+  parameters: [
+    {
+      description: 'The type of apiDoc to return.',
+      in: 'query',
+      name: 'type',
+      type: 'string',
+      enum: [
+        'apiDoc',
+        'operationDoc'
+      ]
+    }
+  ],
+  responses: {
+    200: {
+      description: 'The requested apiDoc.',
+      schema: {
+        type: 'object'
+      }
+    },
+    default: {
+      description: 'The requested apiDoc.',
+    }
+  }
+};
