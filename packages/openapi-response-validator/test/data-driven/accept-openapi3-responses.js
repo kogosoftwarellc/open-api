3db0f0f78590a506c92a01ad330efc827ef37c33
“funduck” <qlfunduck@gmail.com>
2018-12-12 12:07:05 +0300

    openapi-response-validator: accepts openapi3 path to schema responses.CODE.content."application/json".schema

---
+++ packages/openapi-response-validator/test/data-driven/accept-openapi3-responses.js
@@ -0,0 +1,28 @@
+module.exports = {
+  constructorArgs: {
+    responses: {
+      200: {
+        description: "Ok",
+        content: {
+          "application/json": {
+            schema: {
+              type: 'object',
+              properties: {
+                foo: {
+                  type: 'string'
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    definitions: null
+  },
+
+  inputStatusCode: 200,
+
+  inputResponseBody: {foo: 'asdf'},
+
+  expectedValidationError: void 0
+};
