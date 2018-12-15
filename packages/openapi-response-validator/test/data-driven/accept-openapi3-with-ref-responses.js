c8935f7009c66fc0e9cd7581e998853ef3854f3a
Sergey Maximov <sergey.maximov@yld.io>
2018-12-15 17:31:40 +0000

    fix: Fix OpenAPI v3 response validation for schemas with references

---
+++ packages/openapi-response-validator/test/data-driven/accept-openapi3-with-ref-responses.js
@@ -0,0 +1,35 @@
+module.exports = {
+  constructorArgs: {
+    responses: {
+      200: {
+        description: "Ok",
+        content: {
+          "application/json": {
+            schema: {
+              '$ref': '#/components/schemas/Foo'
+            }
+          }
+        }
+      }
+    },
+    components: {
+      schemas: {
+        Foo: {
+          type: 'object',
+          required: ['id'],
+          properties: {
+            id: {
+              type: 'string'
+            }
+          }
+        }
+      }
+    },
+  },
+
+  inputStatusCode: 200,
+
+  inputResponseBody: {id: 'asdf'},
+
+  expectedValidationError: void 0
+};
