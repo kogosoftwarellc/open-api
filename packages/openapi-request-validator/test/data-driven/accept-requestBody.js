9d6e02bc144736faf8f12705e2486f8c6298b4da
William F Wheeler II <bill@quickbrownfoxlabs.com>
2018-12-18 08:58:05 -0500

    validate v3 requestBody

---
+++ packages/openapi-request-validator/test/data-driven/accept-requestBody.js
@@ -0,0 +1,29 @@
+module.exports = {
+  validateArgs: {
+    parameters: [],
+    requestBody: {
+      description: "a test body",
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              foo: {
+                type: 'string'
+              }
+            }
+          }
+        }
+      }
+    },
+    schemas: null
+  },
+  request: {
+    body: {
+      "foo": "asdf"
+    },
+    headers: {
+      'content-type': 'application/json'
+    }
+  }
+};
---
+++ packages/openapi-request-validator/test/data-driven/use-given-media-type-schema-for-requestBody.js
@@ -0,0 +1,46 @@
+module.exports = {
+  "validateArgs": {
+    "parameters": []
+  },
+  requestBody: {
+    "content": {
+      "application/foo1+json": {
+        "schema": {
+          "properties": {
+            "name": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "name"
+          ]
+        }
+      },
+      "application/foo2+json": {
+        "schema": {
+          "properties": {
+            "first_name": {
+              "type": "string"
+            },
+            "last_name": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "first_name",
+            "last_name"
+          ]
+        }
+      }
+    }
+  },
+  "request": {
+    "headers": {
+      "content-type": "application/foo2+json"
+    },
+    "body": {
+      "first_name": "foo",
+      "last_name": "bar"
+    }
+  }
+};
