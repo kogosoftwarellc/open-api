e5f6cc4da63569195ec69841f172b235a14c1ea7
William F Wheeler II <bill@quickbrownfoxlabs.com>
2018-12-20 12:50:03 -0500

    fs-routes: linting and formating

--- packages/fs-routes/test/test.ts
+++ packages/fs-routes/test/test.ts
@@ -4,7 +4,7 @@ const path = require('path');
 const testDir = path.resolve(__dirname, '..', 'test-dir');
 
 function assertRoutes(routes) {
-  var output = [
+  const output = [
     {
       path: testDir + '/home.js',
       route: '/home'
@@ -39,27 +39,33 @@ describe('fs-routes', () => {
 
   describe('when run against a directory twice', () => {
     it('should workd', () => {
-      assertRoutes(fsRoutes('test-dir'));// caching
-      assertRoutes(fsRoutes('test-dir'));// caching
+      assertRoutes(fsRoutes('test-dir')); // caching
+      assertRoutes(fsRoutes('test-dir')); // caching
     });
   });
 
   describe('when run with a file match pattern', () => {
     it('should work', () => {
-      assert.deepEqual(fsRoutes('test-dir', {glob: '**/*.ts', indexFileRegExp: /(?:query)?\.ts$/}), [
-        {
-          path: testDir + '/hom.a.ts',
-          route: '/hom.a'
-        },
-        {
-          path: testDir + '/home.ts',
-          route: '/home'
-        },
-        {
-          path: testDir + '/users/query.ts',
-          route: '/users/'
-        }
-      ]);
+      assert.deepEqual(
+        fsRoutes('test-dir', {
+          glob: '**/*.ts',
+          indexFileRegExp: /(?:query)?\.ts$/
+        }),
+        [
+          {
+            path: testDir + '/hom.a.ts',
+            route: '/hom.a'
+          },
+          {
+            path: testDir + '/home.ts',
+            route: '/home'
+          },
+          {
+            path: testDir + '/users/query.ts',
+            route: '/users/'
+          }
+        ]
+      );
     });
   });
 });
