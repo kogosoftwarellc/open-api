3326ab78a9ef329c17b89368a202af7c1177d658
Christian Knecht <christian.knecht@iao.fraunhofer.de>
2019-01-05 01:17:18 +0100

    openapi-response-validator: Allow different content types for OpenAPI 3.0 response definitions

---
+++ packages/openapi-response-validator/test/data-driven/accept-openapi3-responses-different-content-types.js
@@ -0,0 +1,24 @@
+module.exports = {
+    constructorArgs: {
+      responses: {
+        200: {
+          description: 'Ok',
+          content: {
+            'text/markdown': {
+              schema: {
+                type: 'string'
+              }
+            }
+          }
+        }
+      },
+      definitions: null
+    },
+  
+    inputStatusCode: 200,
+  
+    inputResponseBody: '# documentation\n',
+  
+    expectedValidationError: void 0
+  };
+  
