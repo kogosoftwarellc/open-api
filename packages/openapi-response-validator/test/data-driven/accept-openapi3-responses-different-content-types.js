f96fba0d1a3aeee39b3dee765a2d801534a1b557
Christian Knecht <christian.knecht@iao.fraunhofer.de>
2019-01-05 02:25:57 +0100

    openapi-response-validator: Fix linting issues in accept-openapi3-responses-different-content-types test

--- packages/openapi-response-validator/test/data-driven/accept-openapi3-responses-different-content-types.js
+++ packages/openapi-response-validator/test/data-driven/accept-openapi3-responses-different-content-types.js
@@ -1,24 +1,23 @@
 module.exports = {
-    constructorArgs: {
-      responses: {
-        200: {
-          description: 'Ok',
-          content: {
-            'text/markdown': {
-              schema: {
-                type: 'string'
-              }
+  constructorArgs: {
+    responses: {
+      200: {
+        description: 'Ok',
+        content: {
+          'text/markdown': {
+            schema: {
+              type: 'string'
             }
           }
         }
-      },
-      definitions: null
+      }
     },
-  
-    inputStatusCode: 200,
-  
-    inputResponseBody: '# documentation\n',
-  
-    expectedValidationError: void 0
-  };
-  
+    definitions: null
+  },
+
+  inputStatusCode: 200,
+
+  inputResponseBody: '# documentation\n',
+
+  expectedValidationError: void 0
+};
