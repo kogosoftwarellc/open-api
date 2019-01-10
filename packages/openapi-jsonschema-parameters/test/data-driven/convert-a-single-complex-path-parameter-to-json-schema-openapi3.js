39551a8dcd2ffee6b78e94fa2f25663066b54b84
Maxime Morand <mmorand.ext@orange.com>
2019-01-10 14:20:21 +0100

    openapi-jsonschema-parameters : fix linting errors

--- packages/openapi-jsonschema-parameters/test/data-driven/convert-a-single-complex-path-parameter-to-json-schema-openapi3.js
+++ packages/openapi-jsonschema-parameters/test/data-driven/convert-a-single-complex-path-parameter-to-json-schema-openapi3.js
@@ -1,60 +1,60 @@
 module.exports = {
-    parameters: [
-        {
-            in: 'path',
-            name: 'foo',
-            required: true,
-            // jsonschema properties
-            schema: {
-                default: 5,
-                description: 'asdfasdf',
-                format: 'asdf',
-                type: 'string',
-                title: 'fffasdf',
-                maximum: 0,
-                additionalItems: true,
-                items: [],
-                exclusiveMaximum: true,
-                minimum: 5,
-                exclusiveMinimum: false,
-                maxLength: 5,
-                minLength: 6,
-                pattern: '^asdf$',
-                maxItems: 5,
-                minItems: 7,
-                uniqueItems: false,
-                enum: ['1', '3'],
-                multipleOf: 57
-            }
-        }
-    ],
-
-    outputSchema: {
-        path: {
-            properties: {
-                foo: {
-                    additionalItems: true,
-                    default: 5,
-                    description: 'asdfasdf',
-                    type: 'string',
-                    maximum: 0,
-                    exclusiveMaximum: true,
-                    minimum: 5,
-                    exclusiveMinimum: false,
-                    format: 'asdf',
-                    items: [],
-                    maxLength: 5,
-                    minLength: 6,
-                    pattern: '^asdf$',
-                    title: 'fffasdf',
-                    maxItems: 5,
-                    minItems: 7,
-                    uniqueItems: false,
-                    enum: ['1', '3'],
-                    multipleOf: 57
-                }
-            },
-            required: ['foo']
-        }
+  parameters: [
+    {
+      in: 'path',
+      name: 'foo',
+      required: true,
+      // jsonschema properties
+      schema: {
+        default: 5,
+        description: 'asdfasdf',
+        format: 'asdf',
+        type: 'string',
+        title: 'fffasdf',
+        maximum: 0,
+        additionalItems: true,
+        items: [],
+        exclusiveMaximum: true,
+        minimum: 5,
+        exclusiveMinimum: false,
+        maxLength: 5,
+        minLength: 6,
+        pattern: '^asdf$',
+        maxItems: 5,
+        minItems: 7,
+        uniqueItems: false,
+        enum: ['1', '3'],
+        multipleOf: 57
+      }
     }
+  ],
+
+  outputSchema: {
+    path: {
+      properties: {
+        foo: {
+          additionalItems: true,
+          default: 5,
+          description: 'asdfasdf',
+          type: 'string',
+          maximum: 0,
+          exclusiveMaximum: true,
+          minimum: 5,
+          exclusiveMinimum: false,
+          format: 'asdf',
+          items: [],
+          maxLength: 5,
+          minLength: 6,
+          pattern: '^asdf$',
+          title: 'fffasdf',
+          maxItems: 5,
+          minItems: 7,
+          uniqueItems: false,
+          enum: ['1', '3'],
+          multipleOf: 57
+        }
+      },
+      required: ['foo']
+    }
+  }
 };
