9b785f1c16f4bcd1d8bcfe1fecc041c616fa4f92
Milan Simonovic <mbsimonovic@gmail.com>
2019-01-11 12:47:50 +0100

    openapi-framework: pass v3 schema definitions to request validator (closes #280)

---
+++ packages/openapi-framework/test/sample-projects/resolve-reference-v3/spec.ts
@@ -0,0 +1,30 @@
+/* tslint:disable:no-unused-expression */
+import { expect } from 'chai';
+import OpenapiFramework from '../../../';
+const path = require('path');
+
+describe(path.basename(__dirname), () => {
+  let framework: OpenapiFramework;
+
+  beforeEach(() => {
+    framework = new OpenapiFramework({
+      apiDoc: path.resolve(__dirname, 'apiDoc.yml'),
+      featureType: 'middleware',
+      name: 'some-framework',
+      paths: path.resolve(__dirname, 'paths')
+    });
+  });
+
+  it('should resolve reference for requestBody', () => {
+    framework.initialize({
+      visitOperation(ctx) {
+        expect(ctx.features.requestValidator).to.not.be.undefined;
+        const err = ctx.features.requestValidator.validate({
+          body: { title: 'test' },
+          headers: { 'content-type': 'application/json' }
+        });
+        expect(err).to.be.undefined;
+      }
+    });
+  });
+});
