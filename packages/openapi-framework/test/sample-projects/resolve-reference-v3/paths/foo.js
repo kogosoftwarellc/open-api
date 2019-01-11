080ee7904fb06ffed557c09767d94d86eed82745
Milan Simonovic <mbsimonovic@gmail.com>
2019-01-11 12:47:50 +0100

    openapi-framework: pass v3 schema definitions to request validator (closes #280)

---
+++ packages/openapi-framework/test/sample-projects/resolve-reference-v3/paths/foo.js
@@ -0,0 +1,29 @@
+module.exports = {
+  PUT
+};
+
+function PUT() {
+  return;
+}
+PUT.apiDoc = {
+  requestBody: {
+    content: {
+      'application/json': {
+        schema: {
+          $ref: '#/components/schemas/Document'
+        }
+      }
+    }
+  },
+  responses: {
+    '200': {
+      description: 'return foo',
+      content: {
+        'application/json': {
+          schema: {}
+        }
+      }
+    }
+  },
+  tags: ['testing', 'example']
+};
