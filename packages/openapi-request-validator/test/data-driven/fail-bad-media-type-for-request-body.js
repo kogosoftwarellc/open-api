773be356baa3961aee892e65185942f3ec2a6229
keijokapp <keijo.kapp@gmail.com>
2019-01-26 20:22:22 +0200

    openapi-request-validator: Handle missing or invalid 'Content-Type'

---
+++ packages/openapi-request-validator/test/data-driven/fail-bad-media-type-for-request-body.js
@@ -0,0 +1,32 @@
+module.exports = {
+  validateArgs: {
+    parameters: [],
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            properties: {
+              foo: {
+                type: 'string'
+              }
+            },
+            required: ['foo']
+          }
+        }
+      }
+    }
+  },
+  request: {
+    headers: {
+      'content-type': 'abcdef'
+    }
+  },
+  expectedError: {
+    status: 415,
+    errors: [
+      {
+        message: 'Unsupported Content-Type abcdef'
+      }
+    ]
+  }
+};
---
+++ packages/openapi-request-validator/test/data-driven/handle-missing-content-type-as-missing-body.js
@@ -0,0 +1,33 @@
+module.exports = {
+  validateArgs: {
+    parameters: [],
+    requestBody: {
+      required: true,
+      content: {
+        'application/json': {
+          schema: {
+            properties: {
+              foo: {
+                type: 'string'
+              }
+            },
+            required: ['foo']
+          }
+        }
+      }
+    }
+  },
+  request: {
+    headers: {}
+  },
+  expectedError: {
+    status: 400,
+    errors: [
+      {
+        errorCode: 'required.openapi.validation',
+        message: 'media type is not specified',
+        location: 'body'
+      }
+    ]
+  }
+};
