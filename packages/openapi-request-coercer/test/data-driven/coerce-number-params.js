753114ae60966b06cee490aeb25ee725dee0851c
Manuel Cabral <Manny651@users.noreply.github.com>
2019-02-28 11:14:07 -0500

    Don't coerce into numbers values that cannot be converted (issue #371) (#373)
    * Don't coerce into numbers values that cannot be parsed (issue #371)
    
    * Replaced es6 method
    
    * Linted

---
+++ packages/openapi-request-coercer/test/data-driven/coerce-number-params.js
@@ -0,0 +1,48 @@
+module.exports = {
+  args: {
+    parameters: [
+      {
+        in: 'query',
+        name: 'number1',
+        type: 'number'
+      },
+
+      {
+        in: 'query',
+        name: 'number2',
+        type: 'number'
+      },
+
+      {
+        in: 'query',
+        name: 'integer1',
+        type: 'integer'
+      },
+
+      {
+        in: 'query',
+        name: 'integer2',
+        type: 'integer'
+      }
+    ]
+  },
+
+  request: {
+    query: {
+      number1: '1',
+      number2: 'abc',
+      integer1: '2',
+      integer2: 'def'
+    },
+    headers: null
+  },
+
+  headers: null,
+
+  query: {
+    number1: 1,
+    number2: 'abc',
+    integer1: 2,
+    integer2: 'def'
+  }
+};
