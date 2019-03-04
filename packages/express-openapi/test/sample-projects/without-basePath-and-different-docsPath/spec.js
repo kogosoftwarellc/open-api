cf1663c8d53f50cf3bcb931e3a26c0443d27457f
Manuel Cabral <manuel.cabral@vooban.com>
2019-03-04 12:33:42 -0500

    express-openapi: Set default basePath (issue #376)

--- packages/express-openapi/test/sample-projects/without-basePath-and-different-docsPath/spec.js
+++ packages/express-openapi/test/sample-projects/without-basePath-and-different-docsPath/spec.js
@@ -11,3 +11,13 @@ it('should be mounted at the top level', function(done) {
     .get('/foo-docs')
     .expect(200, done);
 });
+
+it('should set the default basePath', function(done) {
+  request(app)
+    .get('/foo-docs')
+    .expect(200)
+    .end(function(err, res) {
+      expect(res.body.basePath).to.eql('/');
+      done(err);
+    });
+});
