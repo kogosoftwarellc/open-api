2d5cfc5bc105b5ef7a700b31729ba360e0753eed
Javier Blanco Martinez <javier.blanco@axa.com>
2019-03-08 17:56:08 +0100

    openapi-request-validator: Allow to use request validation without parameters (#382)
    * openapi-request-validator: Not throw an error if parameters are not defined
    
    Closes #381
    
    * openapi-request-validator: add a test to check that works without parameters

---
+++ packages/openapi-request-validator/test/data-driven/accept-no-parameters.js
@@ -0,0 +1,32 @@
+module.exports = {
+  validateArgs: {
+    requestBody: {
+      description: 'a test body',
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/components/schemas/Test1'
+          }
+        }
+      }
+    },
+    schemas: {
+      Test1: {
+        properties: {
+          foo: {
+            type: 'string'
+          }
+        },
+        required: ['foo']
+      }
+    }
+  },
+  request: {
+    body: {
+      foo: 'asdf'
+    },
+    headers: {
+      'content-type': 'application/json'
+    }
+  }
+};
