95bbcb5eb8fe951d1ee9de459e56ce3f14938ba3
Tucker Whitehouse <tuckerwhitehouse@gmail.com>
2019-04-03 13:29:05 -0400

    openapi-response-validator: Set "useDefaults: true" on Ajv

---
+++ packages/openapi-response-validator/test/data-driven/accept-valid-responses-with-defaults.js
@@ -0,0 +1,25 @@
+module.exports = {
+  constructorArgs: {
+    responses: {
+      200: {
+        schema: {
+          type: 'object',
+          properties: {
+            foo: {
+              type: 'string',
+              default: 'asdf'
+            }
+          },
+          required: ['foo']
+        }
+      }
+    },
+
+    definitions: null
+  },
+
+  inputStatusCode: 200,
+  inputResponseBody: {},
+
+  expectedValidationError: void 0
+};
