3cbe46145e1e88b89863e4a5d270e4e53cbb8839
javier-blanco <javier.blanco@axa.com>
2019-04-25 17:51:30 +0200

    feat(openapi-request-validator): allow null enums and nullable option
    Closes #397

---
+++ packages/openapi-request-validator/test/data-driven/accept-requestBody-with-nullable-enum.js
@@ -0,0 +1,31 @@
+module.exports = {
+  validateArgs: {
+    parameters: [],
+    requestBody: {
+      description: 'a test body',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              foo: {
+                type: 'string',
+                nullable: true,
+                enum: ['HOME', 'CAR']
+              }
+            }
+          }
+        }
+      }
+    },
+    schemas: null
+  },
+  request: {
+    body: {
+      foo: null
+    },
+    headers: {
+      'content-type': 'application/json'
+    }
+  }
+};
