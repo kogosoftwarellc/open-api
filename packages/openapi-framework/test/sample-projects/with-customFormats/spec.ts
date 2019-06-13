6836231d27df4d3943817e7246ce5e764ea878cf
Matias Grunberg <matias@yellowspot.com.ar>
2019-06-13 15:52:41 -0300

    openapi-framework: Update customFormat argument typing (#471)
    * openapi-framework: fix customFormat argument typing.
    
    Right now it's an object with only one property name 'string'
    
    * Lint code: add new line at the end of the file

---
+++ packages/openapi-framework/test/sample-projects/with-customFormats/spec.ts
@@ -0,0 +1,43 @@
+import { expect } from 'chai';
+import OpenapiFramework from '../../../';
+const path = require('path');
+
+describe(path.basename(__dirname), () => {
+  let framework: OpenapiFramework;
+
+  beforeEach(() => {
+    framework = new OpenapiFramework({
+      apiDoc: path.resolve(__dirname, 'apiDoc.yml'),
+      featureType: 'middleware',
+      name: 'some-framework',
+      paths: path.resolve(__dirname, 'paths')
+    });
+  });
+
+  it('should work', () => {
+    framework.initialize({
+      visitApi(ctx) {
+        const apiDoc = ctx.getApiDoc();
+        expect(apiDoc.paths['/foo']).to.eql({
+          parameters: [],
+          get: {
+            parameters: [
+              {
+                format: 'foo',
+                in: 'query',
+                name: 'foo',
+                type: 'string'
+              }
+            ],
+            responses: {
+              default: {
+                description: 'return foo',
+                schema: {}
+              }
+            }
+          }
+        });
+      }
+    });
+  });
+});
