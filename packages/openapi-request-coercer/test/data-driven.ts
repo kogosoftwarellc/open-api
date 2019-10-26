18dc011f813df560a79dc150e88ead25d6e8fe96
nulltoken <emeric.fermas@gmail.com>
2019-10-26 19:41:40 +0200

    openapi-request-coercer: do not throw on untyped array items
    Although this is recommended by the Swagger 2.0 spec, this is sadly not enforced by the schema
    
    References:
    - https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#itemsObject
    - https://github.com/OAI/OpenAPI-Specification/blob/7ce374d67e46e2fdf0da49fc6e29f6854985627e/schemas/v2.0/schema.json#L1108-L1176

--- packages/openapi-request-coercer/test/data-driven.ts
+++ packages/openapi-request-coercer/test/data-driven.ts
@@ -2,7 +2,42 @@ const expect = require('chai').expect;
 const glob = require('glob');
 const path = require('path');
 const baseDir = path.resolve(__dirname, 'data-driven');
+import { Logger } from 'ts-log';
 import Sut from '../';
+
+interface LogEntry {
+  type: string;
+  message: any | undefined;
+  optionalParams: any[];
+}
+
+class FakeLogger implements Logger {
+  public readonly logEntries: LogEntry[] = [];
+
+  public trace(message?: any, ...optionalParams: any[]): void {
+    this.logEntries.push(this.build('TRACE', message, optionalParams));
+  }
+
+  public debug(message?: any, ...optionalParams: any[]): void {
+    this.logEntries.push(this.build('DEBUG', message, optionalParams));
+  }
+
+  public info(message?: any, ...optionalParams: any[]): void {
+    this.logEntries.push(this.build('INFO', message, optionalParams));
+  }
+
+  public warn(message?: any, ...optionalParams: any[]): void {
+    this.logEntries.push(this.build('WARN', message, optionalParams));
+  }
+
+  public error(message?: any, ...optionalParams: any[]): void {
+    this.logEntries.push(this.build('ERROR', message, optionalParams));
+  }
+
+  private build(type: string, message: any | undefined, optionalParams: any[]) {
+    return { type, message, optionalParams };
+  }
+}
 
 describe(require('../package.json').name, () => {
   glob.sync('*.js', { cwd: baseDir }).forEach(fixture => {
@@ -10,6 +45,11 @@ describe(require('../package.json').name, () => {
     fixture = require(path.resolve(baseDir, fixture));
 
     it(`should ${testName}`, () => {
+      const fakeLogger = new FakeLogger();
+      if (fixture.args) {
+        fixture.args.logger = fakeLogger;
+      }
+
       if (fixture.constructorError) {
         expect(() => {
           /* tslint:disable-next-line:no-unused-expression */
@@ -36,6 +76,12 @@ describe(require('../package.json').name, () => {
 
       if (fixture.body) {
         expect(request.body).to.eql(fixture.body);
+      }
+
+      if (fixture.logs) {
+        expect(fakeLogger.logEntries).to.eql(fixture.logs);
+      } else {
+        expect(fakeLogger.logEntries).to.eql([]);
       }
     });
   });
