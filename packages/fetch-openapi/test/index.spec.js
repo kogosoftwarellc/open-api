f1f67b27eb84e1eade9b5279e3918e8a1ef22728
nulltoken <emeric.fermas@gmail.com>
2019-10-27 21:26:31 +0100

    fixup! Open xplat development to Windows

--- packages/fetch-openapi/test/index.spec.js
+++ packages/fetch-openapi/test/index.spec.js
@@ -24,8 +24,7 @@ describe('fetch-openapi', function() {
       } else {
         it('should output an api service factory', function() {
           expect(sut(require(inputPath), options)).to.equal(
-            fs.readFileSync(outputPath, 'utf8')
-              .replace(/\r\n/g, "\n") // normalize line endings comparisons accross OSes
+            fs.readFileSync(outputPath, 'utf8').replace(/\r\n/g, '\n') // normalize line endings comparisons accross OSes
           );
         });
 
