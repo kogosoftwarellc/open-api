33050c8b2fd4da9310aa62d9df3458ad04a92f73
David Stephens <git@dwrs.org>
2020-07-15 01:50:13 +0100

    openapi-request-coercer: Fix function on Object.prototype being used as coercers (#664)
    Co-authored-by: David Stephens <david.2.stephens@bt.com>

---
+++ packages/openapi-request-coercer/test/data-driven/not-use-Object-prototype-entries-as-coercers.js
@@ -0,0 +1,22 @@
+const customToString = () => "Hello world"
+
+module.exports = {
+  args: {
+    // We're not coercing any params, but there has to be at least one present for the functionality to kick in
+    parameters: [{
+      in: 'path',
+      name: 'path1',
+      type: 'boolean'
+    }]
+  },
+
+  request: {
+    body: {
+      toString: customToString
+    }
+  },
+
+  body: {
+    toString: customToString
+  }
+};
