4bb53c9cc0a8c9c5ed909d710e8b2812d89025fe
Conrad Wandl <conrad.wandl@rise-world.com>
2020-11-09 14:37:28 +0100

    express-openapi: Add docs and tests for custom keywords (refs #628)

---
+++ packages/express-openapi/test/sample-projects/with-customKeywords/app.js
@@ -0,0 +1,50 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+// normally you'd just do require('express-openapi'), but this is for test purposes.
+var openapi = require('../../../');
+var path = require('path');
+var cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+openapi.initialize({
+  apiDoc: require('./api-doc.js'),
+  app: app,
+  paths: path.resolve(__dirname, 'api-routes'),
+  customKeywords: {
+    'x-coerce': {
+      modifying: true,
+      errors: false,
+      validate: function (
+        keywordData,
+        data,
+        parentSchema,
+        dataPath,
+        parentData,
+        parentDataProperty,
+        rootData
+      ) {
+        if (keywordData === 'Date') {
+          const date = new Date(parseInt(data, 10));
+          if (isNaN(date.getTime())) {
+            return false;
+          }
+          parentData[parentDataProperty] = date;
+        }
+        return true;
+      },
+    },
+  },
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status).json(err);
+});
+
+module.exports = app;
+
+const port = parseInt(process.argv[2], 10);
+if (port) {
+  app.listen(port);
+}
