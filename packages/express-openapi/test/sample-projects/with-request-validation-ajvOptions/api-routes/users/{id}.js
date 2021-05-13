aeedf753b05e3d4b1ee0338ddeef226b07924868
Nicolo' Fuccella <nico@soluzionifutura.it>
2021-05-13 11:54:39 +0200

    express-openapi: add tests for requestAjvOptions and responseAjvOptions

---
+++ packages/express-openapi/test/sample-projects/with-request-validation-ajvOptions/api-routes/users/{id}.js
@@ -0,0 +1,54 @@
+module.exports = {
+  // parameters for all operations in this path
+  parameters: [
+    {
+      name: 'id',
+      in: 'path',
+      type: 'string',
+      required: true,
+      description: "Fred's age.",
+    },
+  ],
+  // or they may also be an array of middleware + the method handler.  This allows
+  // for flexible middleware management.  express-openapi middleware generated from
+  // the <path>.parameters + <methodHandler>.apiDoc.parameters is prepended to this
+  // array.
+  post: [
+    function (req, res, next) {
+      next();
+    },
+    function (req, res) {
+      res.status(200).json(req.body);
+    },
+  ],
+};
+
+module.exports.post.apiDoc = {
+  description: 'Create a user.',
+  operationId: 'createUser',
+  tags: ['users'],
+  parameters: [
+    {
+      name: 'user',
+      in: 'body',
+      schema: {
+        $ref: '#/definitions/User',
+      },
+    },
+  ],
+
+  responses: {
+    200: {
+      description: 'Requested user',
+      schema: {
+        $ref: '#/definitions/User',
+      },
+    },
+    default: {
+      description: 'Unexpected error',
+      schema: {
+        $ref: '#/definitions/Error',
+      },
+    },
+  },
+};
