a580ae3dee6af78515732cdfd92b6cbcb9a11c9b
Mehdi Lahlou <mlahlou@protonmail.ch>
2021-08-27 18:31:06 +0200

    koa-openapi: restore `sample-projects` tests & fix `without-basePath` test based on the express version (closes #762) (#763)

--- packages/koa-openapi/test/sample-projects/without-basePath-and-different-docsPath/spec.js
+++ packages/koa-openapi/test/sample-projects/without-basePath-and-different-docsPath/spec.js
@@ -1,7 +1,10 @@
-var app;
-var expect = require('chai').expect;
-var supertest = require('supertest');
+const expect = require('chai').expect;
+const supertest = require('supertest');
 const http = require('http');
+
+let app;
+let request;
+let server;
 
 before(function () {
   app = require('./app.js');
@@ -14,12 +17,12 @@ after(() => {
 });
 
 it('should be mounted at the top level', function (done) {
-  request.get('/api-docs').expect(200, done);
+  request.get('/foo-docs').expect(200, done);
 });
 
 it('should set the default basePath', function (done) {
   request
-    .get('/api-docs')
+    .get('/foo-docs')
     .expect(200)
     .end(function (err, res) {
       expect(res.body.basePath).to.eql('/');
